3f512376471ac5c810bdef8ab6cf52d9897624dc
Jackie Chen <xiaochen.chen@iag.com.au>
2021-11-26 11:41:47 +1100

    Update message

--- index.js
+++ index.js
@@ -6,7 +6,9 @@
 "use strict";
 
 const demoPage =
-  '<html><body><h2>Hi there, this Lambda function is created by S2S!</h2><p id="demo"></p><script>const d = new Date();document.getElementById("demo").innerHTML = d;</script></body></html>';
+  '<html><body><h1>Lambda-Service-Demo</h1>\
+  <h2>Another service proudly deployed by S2S!</h2>\
+  <p id="demo"></p><script>const d = new Date();document.getElementById("demo").innerHTML = d;</script></body></html>';
 
 async function handler(event, context) {
   return {
